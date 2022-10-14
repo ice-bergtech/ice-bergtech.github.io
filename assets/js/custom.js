0cc0e08400cfa5e8dad77c38dff1442ab9d6c8b8
Iceberg <dev@icebergtech.xyz>
2022-10-13 19:16:52 -0700

    Cleanup custom js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,62 +1,3 @@
-(function ($) {
-
-  // Facts counter
-
-  var toggle = document.getElementById("menu-toggle");
-  var menu = document.getElementById("menu");
-  var close = document.getElementById("menu-close");
-
-  toggle.addEventListener("click", function (e) {
-    if (menu.classList.contains("open")) {
-      menu.classList.remove("open");
-    } else {
-      menu.classList.add("open");
-    }
-  });
-
-  close.addEventListener("click", function (e) {
-    menu.classList.remove("open");
-  });
-
-  // Close menu after click on smaller screens
-  $(window).on("resize", function () {
-    if ($(window).width() < 846) {
-      $(".main-menu a").on("click", function () {
-        menu.classList.remove("open");
-      });
-    }
-  });
-
-
-  $(".hover").mouseleave(function () {
-    $(this).removeClass("hover");
-  });
-
-
-
-  $(".isotope-wrapper").each(function () {
-    var $isotope = $(".isotope-box", this);
-    var $filterCheckboxes = $('input[type="radio"]', this);
-
-    var filter = function () {
-      var type = $filterCheckboxes.filter(":checked").data("type") || "*";
-      if (type !== "*") {
-        type = '[data-type="' + type + '"]';
-      }
-      $isotope.isotope({ filter: type });
-    };
-
-    $isotope.isotope({
-      itemSelector: ".isotope-item",
-      layoutMode: "masonry"
-    });
-
-    $(this).on("change", filter);
-    filter();
-  });
-
-})(jQuery);
-
 //Go to top button:
 mybutton = document.getElementById("buttonToTop");
 
@@ -71,10 +12,4 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
